f533414b250f17814df7f9c5f2af379607528199
Ayana0904 <140305652+Ayana0904@users.noreply.github.com>
2023-08-04 17:47:32 +0600

    Delete posts directory

--- posts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class PostCreateForm(forms.Form):
-    image = forms.ImageField(required=False)
-    title = forms.CharField(max_length=128)
-    rate = forms.FloatField()
-    description = forms.CharField(widget=forms.Textarea())
