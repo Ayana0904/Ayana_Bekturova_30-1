7216e51fb92c79f51ed3a3fce7763be291c78d15
Ayana0904 <140305652+Ayana0904@users.noreply.github.com>
2023-08-04 12:42:17 +0600

    Update views.py

--- Product/views.py
+++ Product/views.py
@@ -47,3 +47,76 @@ def categories_view(request):
         return render(request, 'categories/categories.html', context=context_data)
 
 
+def products_detail_view(request, id):
+    if request.method == 'GET':
+        product = Products.objects.get(id=id)
+
+        context = {
+            'product': product,
+            'comments': product.comment_set.all(),
+            'form': CommentsCreateForm
+        }
+
+        return render(request, 'products/detail.html', context=context)
+
+    if request.method == 'POST':
+        product = Products.objects.get(id=id)
+        data = request.POST
+        form = CommentsCreateForm(data=data)
+
+        if form.is_valid():
+            Comment.objects.create(
+                text=form.cleaned_data.get('text'),
+                title=form.cleaned_data.get('name'),
+                product=product
+
+            )
+
+        context = {
+            'product': product,
+            'comments': product.comment_set.all(),
+            'form': form
+        }
+
+        return render(request, 'products/detail.html', context=context)
+
+def create_product_view(request):
+    if request.method == 'GET':
+        context_data = {'form': Products}
+        return render(request, 'products/', context=context_data)
+
+    if request.method == 'POST':
+        data, files = request.POST, request.FILES
+        form = Products_create(data, files)
+        if form.is_valid():
+            Products.objects.create(
+                image=form.cleaned_data.get('image'),
+                title=form.cleaned_data.get('title'),
+                description=form.cleaned_data.get('description'),
+                rate=form.cleaned_data.get('rate'),
+                category=form.cleaned_data.get('category'),
+                prize=form.cleaned_data.get('prize'),
+
+
+            )
+            return redirect('/products/')
+        return render(request, 'products/categories.html', context={'form': form})
+
+
+def create_category_view(request):
+    if request.method == 'GET':
+        context_data = {'form': Category_create}
+
+        return render(request, 'products/categories.html', context=context_data)
+
+    if request.method == 'POST':
+        data, files = request.POST, request.FILES
+        form = Category_create(data, files)
+
+        if f.is_valid():
+            Category.objects.create(
+                title=form.cleaned_data.get('title')
+
+            )
+            return redirect('/category/')
+        return render(request, 'products/categories.html', context={'form': form})
