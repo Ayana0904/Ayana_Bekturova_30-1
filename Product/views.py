06e454b0fab1ba1687c8dd278d70fc0c524fdf6c
Ayana0904 <140305652+Ayana0904@users.noreply.github.com>
2023-08-04 12:27:56 +0600

    Update views.py

--- Product/views.py
+++ Product/views.py
@@ -12,9 +12,25 @@ def main_view(request):
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
+        posts = Post.objects.all().order_by('-create_date')
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
+        if search:
+            products = products.filter(title__contains=search) | products.filter(description__contains=search)
+
+        max_page = posts.__len__() / PAGINATION_LIMIT
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products [PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
+
 
         context_data = {
             'products': products
+            
         }
 
         return render(request, 'products/products.html', context=context_data)
